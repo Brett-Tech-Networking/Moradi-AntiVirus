fc04f2c5cd18f871eed111d895fd6da6ee023afe
matthew17754 <matmoradi17@gmail.com>
2018-07-01 11:45:57 -0700

    Updated Assembly Info
    Version 0.1.0.0

--- Moradi Anti Virus/Properties/AssemblyInfo.cs
+++ Moradi Anti Virus/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Moradi Anti Virus")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Moradi AntiVirus")]
+[assembly: AssemblyDescription("AntiVirus Tool To Defend Yourself Against The Internet")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Moradi Anti Virus")]
+[assembly: AssemblyProduct("Moradi AntiVirus")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
